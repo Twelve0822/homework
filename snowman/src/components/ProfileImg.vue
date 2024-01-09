849878ec98caeef55b85a74e38c91714adea1ef9
WANG SHAOYU <wsysuper@gmail.com>
2024-01-09 12:33:07 +0900

    change html to vue.js (#1)
    * fix html and css
    
    * change html to vue.js
    
    * remove vue logo

---
+++ snowman/src/components/ProfileImg.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { useImglinkStore } from '../stores/imglink'
+
+const store = useImglinkStore()
+</script>
+
+<template>
+  <img alt="logo" class="logo" :src="store.imglink" height="250" />
+</template>
+
+<style scoped>
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+</style>
